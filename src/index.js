9197288aec1f36f94a9924c58179885ef9d2f9cf
Kareem-Adam <82541086+Kareem-Adam@users.noreply.github.com>
2021-11-24 16:15:58 +0000

    feat: implement game history traverse

--- src/index.js
+++ src/index.js
@@ -62,6 +62,7 @@ class Game extends React.Component {
     this.state = {
       history: [{ squares: Array(9).fill(null), }],
       xIsNext: true,
+      stepNumber:0,
     }
   }
 
@@ -73,7 +74,7 @@ class Game extends React.Component {
       - easy to detect changes 
       - allows react to determine when to re-render
     */
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     /* if game has not been won or square empty */
@@ -82,44 +83,41 @@ class Game extends React.Component {
       this.setState({
         /* concats joins current array to history */
         history: history.concat([{ squares: squares, }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     } else {
       return;
     }
   }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? 
-      'Go to move #' + move : 
-      'Go to game start';
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
 
       return (
-        <li>
-          <button 
-          onClick={()=> this.jumpTo(move)}>
+        <li key={move}>
+          <button
+            onClick={() => this.jumpTo(move)}>
             {desc}
           </button>
-
-
-
         </li>
-
-
       )
-
-
-
     })
-
-
-
-
-
     let status;
 
     if (winner) { status = 'Winner: ' + winner; }
